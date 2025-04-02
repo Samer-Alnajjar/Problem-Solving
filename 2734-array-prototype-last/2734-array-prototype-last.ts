4b0648ee856943fa9f468fd9e6aa7e4f717679e0
Samer Alnajjar <44429186+Samer-Alnajjar@users.noreply.github.com>
2025-04-02 15:37:28 +0300

    Time: 54 ms (28.72%), Space: 55.5 MB (26.62%) - LeetHub

--- 2734-array-prototype-last/2734-array-prototype-last.ts
+++ 2734-array-prototype-last/2734-array-prototype-last.ts
@@ -3,7 +3,7 @@ interface Array<T> {
 }
 
 Array.prototype.last = function() {
-    return this.length > 0 ? this.splice(-1)[0] : -1
+    return this.length > 0 ? this[this.length - 1] : -1
 };
 
 /**
