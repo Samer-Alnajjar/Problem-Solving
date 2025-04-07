305c5a4317c342a5d73aa48866eeafb18f9c4e0c
Samer Alnajjar <44429186+Samer-Alnajjar@users.noreply.github.com>
2025-04-07 21:17:36 +0300

    Time: 51 ms (57.51%), Space: 56.4 MB (34%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.ts
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.ts
@@ -4,9 +4,7 @@ function reduce(nums: number[], fn: Fn, init: number): number {
     let value = init;
 
     for(let i = 0 ; i < nums.length; i++) {
-        console.log(value);
-        console.log(init);
-        value = fn((value || init), nums[i])
+        value = fn((value), nums[i])
     }
 
     return value;
