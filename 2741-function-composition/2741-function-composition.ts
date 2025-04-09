2416a1e45c86300fea5426cce5ffe6b4925a751c
Samer Alnajjar <44429186+Samer-Alnajjar@users.noreply.github.com>
2025-04-09 15:07:46 +0300

    Time: 43 ms (99.18%), Space: 55.4 MB (76.24%) - LeetHub

--- 2741-function-composition/2741-function-composition.ts
+++ 2741-function-composition/2741-function-composition.ts
@@ -1,11 +1,10 @@
 type F = (x: number) => number;
 
 function compose(functions: F[]): F {
-    
     return function(x: number): number {
-        return functions.reduceRight((acc, func) => func(acc), x)
+        return functions.reduceRight((acc, fn) => fn(acc), x);
     }
-};
+}
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
