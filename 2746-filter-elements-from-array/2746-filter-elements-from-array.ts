1c7901bc05b5b96804eb15e60bf45eb289de391c
Samer Alnajjar <44429186+Samer-Alnajjar@users.noreply.github.com>
2025-04-15 13:33:32 +0300

    Time: 48 ms (53.08%), Space: 55.5 MB (58.34%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.ts
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.ts
@@ -1,5 +1,12 @@
 type Fn = (n: number, i: number) => any
 
 function filter(arr: number[], fn: Fn): any {
-    return arr.filter((num, i) => fn(num, i))
+    let newArr = [];
+    for(let i = 0 ; i < arr.length; i++) {
+        if(fn(arr[i], i)) {
+            newArr[newArr.length] = arr[i]
+        }
+    }
+
+    return newArr;
 };
