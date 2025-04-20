392656fc1e88f6f890966ad195f0e9408c1cab27
Samer Alnajjar <44429186+Samer-Alnajjar@users.noreply.github.com>
2025-04-20 10:00:31 +0300

    Time: 46 ms (55.86%), Space: 55 MB (75.99%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.ts
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.ts
@@ -1,10 +1,7 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
     let newArr = [];
-    let i = 0;
-
-    while(i < arr.length) {
+    for(let i = 0 ; i < arr.length; i++) {
         newArr.push(fn(arr[i], i))
-        i++;
     }
 
     return newArr;
