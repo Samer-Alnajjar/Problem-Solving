43c0a879e272470268b714a8a9a056b295e4e5bf
Samer Alnajjar <samerk.alnajjar@gmail.com>
2025-06-26 06:15:19 +0000

    tournament winner in python

---
+++ tournament-winner/tournamentWinner.py
@@ -0,0 +1,21 @@
+def tournamentWinner(competitions, result):
+    winner = ''
+    scores = {winner: 0}
+    currentWinner = ''
+
+    for i in range(len(competitions)):
+        home, way = competitions[i]
+
+        currentWinner = home if result[i] == 1 else way
+
+        scores.get(currentWinner, 0) + 3
+
+        if(scores[currentWinner] > scores[winner]):
+            winner = currentWinner
+
+    return winner
+
+
+
+
+print(tournamentWinner([['html', 'css'], ['js', 'java'], ['css', 'js']], [1, 1, 0]))
