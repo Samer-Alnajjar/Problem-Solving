4e2a64107b621d7f2cf99f74eac90c0223b72aec
Samer Alnajjar <samerk.alnajjar@gmail.com>
2025-09-21 07:01:31 +0000

    max-sum-subarray python

--- GP-max-sum-subarrays/GP-max-sum-subarrays.py
+++ GP-max-sum-subarrays/GP-max-sum-subarrays.py
@@ -1,16 +1,16 @@
 def max_sum(arr, k):
-    currect_max = 0
+    current_max = 0
     start = 0
     sum = 0
 
     for num, index in enumerate(arr):
         sum+= num
         if(index >= k-1):
-            currect_max = max(currect_max, sum)
+            current_max = max(current_max, sum)
             sum-= arr[start]
             start+= 1
 
-    return currect_max
+    return current_max
 
 
 print(max_sum([2, 1, 5, 1, 3, 2], 3))
