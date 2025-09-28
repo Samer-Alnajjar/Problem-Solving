e276d6ff5360fdf90aa479c385c86e12bd15e756
Samer Alnajjar <samerk.alnajjar@gmail.com>
2025-09-28 06:45:31 +0000

    noRepeatString

--- GP-no-repeat-string/GP-no-repeat-string.js
+++ GP-no-repeat-string/GP-no-repeat-string.js
@@ -1,7 +1,7 @@
-function noRepeatString(str) {
+function noRepeatIndex(str) {
     var max = 0, len = 0, start = 0, obj = {};
     for (var end = 0; end < str.length; end++) {
-        if (!obj[str[end]]) {
+        if (obj[str[end]] == undefined) {
             obj[str[end]] = end;
             len++;
         }
@@ -10,7 +10,30 @@ function noRepeatString(str) {
             obj[str[end]] = end;
             len = end - start + 1;
         }
-        max = Math.max(max, len);
+        max = Math.max(len, max);
     }
     return max;
 }
+// Second solution using counting + shrinking (slower)
+function noRepeatCounting(str) {
+    var max = 0, len = 0, start = 0, obj = {};
+    for (var end = 0; end < str.length; end++) {
+        obj[str[end]] = (obj[str[end]] || 0) + 1;
+        len++;
+        while (obj[str[end]] > 1) {
+            obj[str[end]]--;
+            len--;
+            start++;
+        }
+        max = Math.max(len, max);
+    }
+    return max;
+}
+// const testString = 'abcdefghijklmnopqrstuvwxyz'.repeat(50000);
+// // length = 1.3 million characters
+// console.time('Counting+Shrinking');
+// console.log('Result:', noRepeatCounting(testString));
+// console.timeEnd('Counting+Shrinking');
+// console.time('Index-Tracking');
+// console.log('Result:', noRepeatIndex(testString));
+// console.timeEnd('Index-Tracking');
--- GP-no-repeat-string/GP-no-repeat-string.ts
+++ GP-no-repeat-string/GP-no-repeat-string.ts
@@ -1,21 +1,54 @@
-function noRepeatString(str: string): number {
+function noRepeatIndex(str: string) {
     let max = 0,
         len = 0,
         start = 0,
-        obj: {[key:string]: number} = {}
+        obj: { [key: string]: number } = {};
 
-    for(let end = 0; end < str.length; end++) {
-        if(!obj[str[end]]) {
+    for (let end = 0; end < str.length; end++) {
+        if (obj[str[end]] == undefined) {
             obj[str[end]] = end;
-            len++
+            len++;
         } else {
-            start = Math.max(start, obj[str[end]] + 1)
+            start = Math.max(start, obj[str[end]] + 1);
             obj[str[end]] = end;
-            len = end-start + 1
+            len = end - start + 1;
         }
 
-        max= Math.max(max, len)
+        max = Math.max(len, max);
+    }
+    return max;
+}
+
+// Second solution using counting + shrinking (slower)
+
+function noRepeatCounting(str: string) {
+    let max = 0,
+        len = 0,
+        start = 0,
+        obj: { [key: string]: number } = {};
+
+    for (let end = 0; end < str.length; end++) {
+        obj[str[end]] = (obj[str[end]] || 0) + 1;
+        len++;
+
+        while (obj[str[end]] > 1) {
+            obj[str[end]]--;
+            len--;
+            start++;
+        }
+        max = Math.max(len, max);
     }
 
-    return max
+    return max;
 }
+
+// const testString = 'abcdefghijklmnopqrstuvwxyz'.repeat(50000);
+// // length = 1.3 million characters
+
+// console.time('Counting+Shrinking');
+// console.log('Result:', noRepeatCounting(testString));
+// console.timeEnd('Counting+Shrinking');
+
+// console.time('Index-Tracking');
+// console.log('Result:', noRepeatIndex(testString));
+// console.timeEnd('Index-Tracking');
