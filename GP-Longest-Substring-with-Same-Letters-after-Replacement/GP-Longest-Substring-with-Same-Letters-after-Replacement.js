c7ab28938be3a87012e4d44caff89bd8020d3e7a
Samer Alnajjar <samerk.alnajjar@gmail.com>
2025-09-30 04:23:14 +0000

    Refactor longestSubstring function to improve variable naming and logic clarity

--- GP-Longest-Substring-with-Same-Letters-after-Replacement/GP-Longest-Substring-with-Same-Letters-after-Replacement.js
+++ GP-Longest-Substring-with-Same-Letters-after-Replacement/GP-Longest-Substring-with-Same-Letters-after-Replacement.js
@@ -1,13 +1,15 @@
-function longestSubstring(str, k) {
-    var start = 0, max = 0, obj = {}, repeatedChars = 0;
+function longestSubString(str, k) {
+    var len = 0, start = 0, max = 0, maxRepeated = 0, obj = {};
     for (var end = 0; end < str.length; end++) {
         obj[str[end]] = (obj[str[end]] || 0) + 1;
-        repeatedChars = Math.max(repeatedChars, obj[str[end]]);
-        while (end - start + 1 - repeatedChars > k) {
-            obj[start]--;
+        len++;
+        maxRepeated = Math.max(maxRepeated, obj[str[end]]);
+        while (len - maxRepeated > k) {
+            obj[str[start]]--;
             start++;
+            len--;
         }
-        max = Math.max(max, end - start + 1);
+        max = Math.max(max, len);
     }
     return max;
 }
--- GP-Longest-Substring-with-Same-Letters-after-Replacement/GP-Longest-Substring-with-Same-Letters-after-Replacement.ts
+++ GP-Longest-Substring-with-Same-Letters-after-Replacement/GP-Longest-Substring-with-Same-Letters-after-Replacement.ts
@@ -1,19 +1,22 @@
-function longestSubstring(str: string, k: number): number {
-    let start = 0,
+function longestSubString(str: string, k: number) {
+    let len = 0,
+        start = 0,
         max = 0,
-        obj: { [key: string]: number } = {},
-        repeatedChars = 0;
+        maxRepeated = 0,
+        obj: { [key: string]: number } = {};
 
     for (let end = 0; end < str.length; end++) {
         obj[str[end]] = (obj[str[end]] || 0) + 1;
+        len++;
+        maxRepeated = Math.max(maxRepeated, obj[str[end]]);
 
-        repeatedChars = Math.max(repeatedChars, obj[str[end]]);
-
-        while (end - start + 1 - repeatedChars > k) {
-            obj[start]--;
+        while (len - maxRepeated > k) {
+            obj[str[start]]--;
             start++;
+            len--;
         }
-        max = Math.max(max, end - start + 1);
+
+        max = Math.max(max, len);
     }
 
     return max;
