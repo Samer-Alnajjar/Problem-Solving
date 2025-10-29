9ba8d009f0a8e0988f0c939011d9bafbd6395c96
Samer Alnajjar <samerk.alnajjar@gmail.com>
2025-10-29 14:04:02 +0000

    GP-middle-of-linked-list

--- GP-middle-of-linked-list/GP-middle-of-linked-list.js
+++ GP-middle-of-linked-list/GP-middle-of-linked-list.js
@@ -13,3 +13,14 @@ function middleOfLinkedList(head) {
     }
     return slow;
 }
+// Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+var head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+console.log("Middle Node: ".concat(find_middle_of_linked_list(head).value));
+head.next.next.next.next.next = new Node(6);
+console.log("Middle Node: ".concat(find_middle_of_linked_list(head).value));
+head.next.next.next.next.next.next = new Node(7);
+console.log("Middle Node: ".concat(find_middle_of_linked_list(head).value));
