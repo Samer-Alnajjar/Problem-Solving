10869dc069b69b0619a888bfa872da02d054beea
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2026-02-16 21:52:09 +0300

    insert-interval, solved in a new way

--- GP-insert-interval/GP-insert-interval.ts
+++ GP-insert-interval/GP-insert-interval.ts
@@ -17,11 +17,13 @@ function insert(intervals: Interval[], newInterval: Interval) {
     let merged = [],
         i = 0;
 
+    // No overlap
     while(i < intervals.length && intervals[i].end < newInterval.start) {
         merged.push(intervals[i]);
         i++;
     }
 
+    // There is an overlap
     while(i < intervals.length && intervals[i].start <= newInterval.end) {
         newInterval.start = Math.min(newInterval.start, intervals[i].start)
         newInterval.end = Math.max(newInterval.end, intervals[i].end)
@@ -36,6 +38,29 @@ function insert(intervals: Interval[], newInterval: Interval) {
     }
 
     return merged
+}
+
+function insertInterval(interval: Interval[], newInt: Interval) {
+    let mergedInterval = [];
+
+    for (let i = 0; i < interval.length; i++) {
+        // No overlap, insert before
+        if (interval[i].end < newInt.start) {
+            mergedInterval.push(interval[i])
+        } else if (newInt.end < interval[i].start) {
+            // No overlap, insert after
+            mergedInterval.push(newInt);
+            newInt = interval[i];
+        } else {
+            // There is an overlap
+            newInt.start = Math.min(newInt.start, interval[i].start);
+            newInt.end = Math.max(newInt.end, interval[i].end);
+        }
+    }
+
+    mergedInterval.push(newInt)
+
+    return mergedInterval;
 }
 
 console.log('Intervals after inserting the new interval: ');
